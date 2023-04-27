f5d99106b9013b35ba0eebefa1a8b1e6c4d9c6db
joelvonrotz <joel.vonrotz@stud.hslu.ch>
2023-04-28 01:57:48 +0200

    implemented Classification using CNN (Task 6.2)

--- image_processing.cpp
+++ image_processing.cpp
@@ -2,12 +2,19 @@
 
 #include "image_processing.h"
 
+#define TEST_MODE
 
 CImageProcessor::CImageProcessor() {
 	for(uint32 i=0; i<3; i++) {
 		/* index 0 is 3 channels and indicies 1/2 are 1 channel deep */
 		m_proc_image[i] = new cv::Mat();
 	}
+
+	#ifdef TEST_MODE
+		m_net = cv::dnn::readNetFromONNX("./MnistCNN_Gray.onnx");
+	#else
+		m_net = cv::dnn::readNetFromONNX("/home/pi/CNNdir/MnistCNN_Gray.onnx");
+	#endif
 }
 
 CImageProcessor::~CImageProcessor() {
@@ -24,105 +31,102 @@ cv::Mat* CImageProcessor::GetProcImage(uint32 i) {
 }
 
 
-static int counter;
+static int maxSize = 50;
+static int minSize = 20;
 
 int CImageProcessor::DoProcess(cv::Mat* image) {
-	if(!image) return(EINVALID_PARAMETER);	
-	static cv::Mat grayImage, mPrevImage, ResultImage, mBackgroundImage, mBackgroundImage2; // should be put into class in image_processing.h
-	static bool init = false;
-	int threshold = 35;
-  double alpha = 0.5;
+	if(!image) return(EINVALID_PARAMETER);
+
+	static cv::Mat grayImage,colorImage, imgCanny, binaryImage;
+
+#ifdef TEST_MODE
+	// get test image
+	*image = cv::imread("./MnistRaspiImage_c.png", cv::IMREAD_GRAYSCALE);
+#endif
 
 	if(image->channels() > 1) {
-		cv::cvtColor( *image, grayImage, cv::COLOR_RGB2GRAY);
+		cv::cvtColor(*image, grayImage, cv::COLOR_RGB2GRAY);
+		colorImage = *image;
 	} else {
 		grayImage = *image;
+		cv::cvtColor(*image, colorImage, cv::COLOR_GRAY2RGB);
 	}
 
-	if(!init) {
-		mBackgroundImage2 = grayImage.clone();
-		mBackgroundImage = grayImage.clone();
-		//mBackgroundImage = cv::imread("dx.png");
-		//cv::cvtColor( mBackgroundImage, mBackgroundImage, cv::COLOR_RGB2GRAY);
-		init = true;
-	}
-	cv::addWeighted(mBackgroundImage2, alpha, grayImage, 1-alpha, 0, mBackgroundImage2);
+	// prepare image for contour calculation
+	double threshold1 = 50;
+	double threshold2 = 200;
+	cv::Canny(grayImage, imgCanny, threshold1, threshold2, 3, true);
+	cv::Mat kernel = cv::Mat::ones(5, 5, CV_8UC1);
+	cv::morphologyEx(imgCanny, binaryImage, cv::MORPH_DILATE, kernel);
+	cv::Mat stats, centroids, labelImage;
+	connectedComponentsWithStats(binaryImage, labelImage, stats, centroids);
 
-	counter = (counter + 1) % 200; // after 200 images, apply a new background image (helps with testing out stuff at different locations)
-	if(counter == 0) {
-		mBackgroundImage = mBackgroundImage2.clone();
-		printf("Background Update\n");
-	}
+	cv::Mat resultImage = colorImage.clone();
 
-	*m_proc_image[1] = mBackgroundImage.clone();
+	for(int i = 1; i < stats.rows ; i++) {
+		int topLeftx = stats.at<int>(i, 0);
+		int topLefty = stats.at<int>(i, 1);
+		int width = 		stats.at<int>(i,2);
+		int height = 		stats.at<int>(i,3);
+		int area = 			stats.at<int>(i,4);
 
-	ResultImage = image->clone();
+		if((minSize <= width || minSize <= height) &&
+				width < maxSize && height < maxSize) {
+					
+			int sizeCrop = (13*std::max(width, height))/10;
+			int topLeftxCrop = std::max(0, topLeftx+(width-sizeCrop)/2);
+			int topLeftyCrop = std::max(0, topLefty+(height-sizeCrop)/2);
 
-	//if(mBackgroundImage.size() != cv::Size()) {
-		// calculate absolute difference + thresholding
-		cv::Mat diffImage, binaryImage;
-		cv::absdiff(mBackgroundImage, grayImage, diffImage);
-		cv::threshold(diffImage, binaryImage, threshold, 255, cv::THRESH_BINARY);
+			int widthCrop = std::min(sizeCrop, binaryImage.cols-topLeftxCrop);
+			int heightCrop = std::min(sizeCrop, binaryImage.rows-topLeftyCrop);
 
-		// close gaps
-		cv::Mat kernel = cv::Mat::ones(5,5, CV_8UC1);
-		cv::morphologyEx(binaryImage, binaryImage, cv::MORPH_CLOSE, kernel);
+			cv::Rect rect(topLeftxCrop, topLeftyCrop, widthCrop, heightCrop);
+			cv::rectangle(resultImage, rect, cv::Scalar(255, 0, 0));
 
+			cv::Mat mnistImage = grayImage(rect);
+			mnistImage = 255 - mnistImage; // invert image
+		
+#ifdef TEST_MODE
+			cv::imwrite("./mnistImage.png", mnistImage);
+#endif
+		
+			double min, max;
+			cv::minMaxLoc(mnistImage, &min, &max);
 
-		std::vector<std::vector<cv::Point>> contours;
- 		std::vector<cv::Vec4i> hierarchy;
+			cv::Size classRectSize = cv::Size(28,28);
 
-		cv::findContours(binaryImage, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+			cv::Mat blob = cv::dnn::blobFromImage(mnistImage, 1./(max-min), classRectSize, cv::Scalar(min));
+			
+			m_net.setInput(blob);
+			
+			cv::Mat output = m_net.forward();
 
-		for (unsigned int idx = 0 ; idx < contours.size() ; idx++) {
-			// area
-			double area = cv::contourArea(contours[idx]);
+			int maxDigit = 0;
+			double maxVal = output.at<float>(0,0);
+			for(int i0 = 0; i0 < output.cols; i0++) {
+				std::cout << i0 << "," << output.at<float>(0,i0) << std::endl;
 
-			// bounding rectangle
-			cv::Rect rect = cv::boundingRect(contours[idx]);
-
-			// center of gravity
-			cv::Moments moment = cv::moments(contours[idx]);
-			double cx = moment.m10 / moment.m00;
-			double cy = moment.m01 / moment.m00;
-
-			//to draw counter to index idx in image
-			if(area > 200.0) {
-				cv::drawContours(ResultImage, contours, idx, cv::Scalar(255), 1, 8);
-				cv::rectangle(ResultImage, rect, cv::Scalar(255,0,0));
-				cv::Point2d cent(cx, cy);
-				cv::circle(ResultImage, cent, 5, cv::Scalar(128,0,0), -1);
+				if(maxVal < output.at<float>(0,i0)) {
+					maxVal = output.at<float>(0,i0);
+					maxDigit = i0;
+				}
 			}
+
+			std::string strVal = std::to_string(maxDigit);
+			putText(resultImage, strVal.c_str(), cv::Point(topLeftxCrop,
+				topLeftyCrop-5), cv::HersheyFonts::FONT_HERSHEY_SIMPLEX, 0.8,
+				cv::Scalar(255, 0, 0), 2);
 		}
-		// Labeling
-		//cv::Mat stats, centroids, labelImage;
-		//cv::connectedComponentsWithStats(binaryImage, labelImage, stats, centroids);
+	}
 
-		// draw boxes into image
-		//for(int i = 1 ; i < stats.rows ; i++) {
-		//	int topLeftx = stats.at<int>(i,0);
-		//	int topLefty = stats.at<int>(i,1);
-		//	int width 	 = stats.at<int>(i,2);
-		//	int height   = stats.at<int>(i,3);
-		//	int area     = stats.at<int>(i,4);
-		//	double cx = centroids.at<double>(i,0);
-		//	double cy = centroids.at<double>(i,1);
-		//	if(area > 100) {
-		//		cv::Rect rect(topLeftx, topLefty, width, height);
-		//		cv::rectangle(ResultImage, rect, cv::Scalar(255,0,0));
-		//		cv::Point2d cent(cx, cy);
-		//		cv::circle(ResultImage, cent, 5, cv::Scalar(128,0,0), -1);
-		//	}
-		//}
-	//}
-	//mPrevImage = grayImage.clone();
-	*m_proc_image[0] = ResultImage.clone();
-	*m_proc_image[1] = diffImage.clone();
-	*m_proc_image[2] = mBackgroundImage2.clone();
+#ifdef TEST_MODE
+	*image = cv::imwrite("ResultImage.png", resultImage);
+#endif
 
+	*m_proc_image[0] = resultImage.clone();
+	//*m_proc_image[1] = diffImage.clone();
+	//*m_proc_image[2] = mBackgroundImage2.clone();
 
-  //cv::imwrite("dx.png", mBackgroundImage);
-      //  cv::imwrite("dy.png", *m_proc_image[1]);
 
 	return(SUCCESS);
 }
